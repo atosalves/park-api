110e96e6d85850ed0058809096e326d6936aaa4f
Atos Alves <atosalvesqueiroz@gmail.com>
2025-06-25 15:17:55 -0300

    feat: adiciona endpoint para obter usuário por ID

--- src/main/java/com/atosalves/park_api/web/controller/UserController.java
+++ src/main/java/com/atosalves/park_api/web/controller/UserController.java
@@ -2,6 +2,8 @@ package com.atosalves.park_api.web.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +25,12 @@ public class UserController {
         public ResponseEntity<User> createUser(@RequestBody User user) {
                 var createdUser = userService.createUser(user);
                 return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+        }
+
+        @GetMapping("/{id}")
+        public ResponseEntity<User> getUserById(@PathVariable Long id) {
+                var user = userService.getUserById(id);
+                return ResponseEntity.status(HttpStatus.OK).body(user);
         }
 
 }
