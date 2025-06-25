d59a0bee14dda9e0452181e88cf32eddd558d4ab
Atos Alves <atosalvesqueiroz@gmail.com>
2025-06-25 15:32:25 -0300

    feat: adiciona endpoint para atualização de senha

--- src/main/java/com/atosalves/park_api/web/controller/UserController.java
+++ src/main/java/com/atosalves/park_api/web/controller/UserController.java
@@ -3,6 +3,7 @@ package com.atosalves.park_api.web.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,15 +23,21 @@ public class UserController {
         private final UserService userService;
 
         @PostMapping
-        public ResponseEntity<User> createUser(@RequestBody User user) {
-                var createdUser = userService.createUser(user);
+        public ResponseEntity<User> create(@RequestBody User user) {
+                var createdUser = userService.create(user);
                 return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
         }
 
         @GetMapping("/{id}")
-        public ResponseEntity<User> getUserById(@PathVariable Long id) {
-                var user = userService.getUserById(id);
-                return ResponseEntity.status(HttpStatus.OK).body(user);
+        public ResponseEntity<User> getById(@PathVariable Long id) {
+                var user = userService.getById(id);
+                return ResponseEntity.ok(user);
+        }
+
+        @PatchMapping("/{id}")
+        public ResponseEntity<User> updatePassword(@PathVariable Long id, @RequestBody User user) {
+                var updatedUser = userService.updatePassword(id, user.getPassword());
+                return ResponseEntity.ok(updatedUser);
         }
 
 }
