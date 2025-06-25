6c9d8c1630eafa5ebf63b5235228675f9b416a48
Atos Alves <atosalvesqueiroz@gmail.com>
2025-06-25 15:21:44 -0300

    feat: implementa serviço para busca de usuários por ID
    - ajusta a injeção de dependência do repositório de usuários
    - corrige a anotação de transação

--- src/main/java/com/atosalves/park_api/service/UserService.java
+++ src/main/java/com/atosalves/park_api/service/UserService.java
@@ -1,22 +1,27 @@
 package com.atosalves.park_api.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.atosalves.park_api.entity.User;
 import com.atosalves.park_api.repository.UserRepository;
 
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
 
-        private UserRepository userRepository;
+        private final UserRepository userRepository;
 
         @Transactional
         public User createUser(User user) {
                 return userRepository.save(user);
+        }
+
+        @Transactional(readOnly = true)
+        public User getUserById(Long id) {
+                return userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
         }
 
 }
