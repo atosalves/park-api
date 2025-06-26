495f157097baf6ed5611099f881e5ca08fdef8e5
Atos Alves <atosalvesqueiroz@gmail.com>
2025-06-26 17:27:42 -0300

    chore: adiciona scripts SQL para inserção e remoção de usuários

---
+++ src/test/resources/sql/users/users-insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (id, username, password, role) VALUES (100, 'test1', '123456', 'ROLE_ADMIN');
+INSERT INTO users (id, username, password, role) VALUES (101, 'test2', '123456', 'ROLE_CUSTOMER');
+INSERT INTO users (id, username, password, role) VALUES (102, 'test3', '123456', 'ROLE_CUSTOMER');
