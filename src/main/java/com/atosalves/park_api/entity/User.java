24f4284b43cc1e61c6445b674fd05c41d5e0a5e4
Atos Alves <atosalvesqueiroz@gmail.com>
2025-06-26 10:23:37 -0300

    feat: define valor padrão para o campo role na entidade User

--- src/main/java/com/atosalves/park_api/entity/User.java
+++ src/main/java/com/atosalves/park_api/entity/User.java
@@ -26,7 +26,7 @@ public class User {
 
         @Enumerated(EnumType.STRING)
         @Column(nullable = false, length = 25)
-        private Role role;
+        private Role role = Role.ROLE_CUSTOMER;
 
         private LocalDateTime createdAt;
         private LocalDateTime updatedAt;
