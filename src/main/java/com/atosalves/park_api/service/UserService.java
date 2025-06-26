ba86d47f0bf69469da8ab978417c3c3f4769c2d9
Atos Alves <atosalvesqueiroz@gmail.com>
2025-06-26 12:00:56 -0300

    feat: adiciona tratamento de exceções personalizadas no UserService

--- src/main/java/com/atosalves/park_api/service/UserService.java
+++ src/main/java/com/atosalves/park_api/service/UserService.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.atosalves.park_api.entity.User;
+import com.atosalves.park_api.exception.EntityNotFoundException;
+import com.atosalves.park_api.exception.PasswordInvalidException;
+import com.atosalves.park_api.exception.UniqueViolationException;
 import com.atosalves.park_api.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -18,25 +21,31 @@ public class UserService {
 
         @Transactional
         public User create(User user) {
-                return userRepository.save(user);
+                try {
+                        return userRepository.save(user);
+                } catch (Exception e) {
+                        throw new UniqueViolationException(
+                                        String.format("Nome de usuário '%s' já cadastrado", user.getUsername()));
+                }
         }
 
         @Transactional(readOnly = true)
         public User getById(Long id) {
-                return userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+                return userRepository.findById(id).orElseThrow(
+                                () -> new EntityNotFoundException(String.format("Usuário '%s' não encontrado", id)));
         }
 
         @Transactional
         public void updatePassword(Long id, String currentPassword, String updatedPassword, String confirmedPassword) {
 
                 if (updatedPassword.equals(confirmedPassword)) {
-                        throw new RuntimeException("Nova senha não é igual a senha de confirmação");
+                        throw new PasswordInvalidException("Nova senha não é igual a senha de confirmação");
                 }
 
                 var user = getById(id);
 
                 if (user.getPassword().equals(currentPassword)) {
-                        throw new RuntimeException("Senha não confere");
+                        throw new PasswordInvalidException("Senha não confere");
                 }
 
                 user.setPassword(updatedPassword);
