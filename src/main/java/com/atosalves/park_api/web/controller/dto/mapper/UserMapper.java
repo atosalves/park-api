7a68eaa11a5994caa94d335ff7b47ccfd7cd2b96
Atos Alves <atosalvesqueiroz@gmail.com>
2025-06-26 10:22:27 -0300

    feat: adiciona classe UserMapper para conversão entre DTOs e entidade User

---
+++ src/main/java/com/atosalves/park_api/web/controller/dto/mapper/UserMapper.java
@@ -0,0 +1,28 @@
+package com.atosalves.park_api.web.controller.dto.mapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.atosalves.park_api.entity.User;
+import com.atosalves.park_api.web.controller.dto.user.UserCreateDto;
+import com.atosalves.park_api.web.controller.dto.user.UserResponseDto;
+
+public class UserMapper {
+
+        public static User toUser(UserCreateDto createDto) {
+                var user = new User();
+
+                return user;
+        }
+
+        public static UserResponseDto toDto(User user) {
+                String role = user.getRole().name().substring("ROLE_".length());
+
+                return new UserResponseDto(user.getId(), user.getUsername(), role);
+        }
+
+        public static List<UserResponseDto> toListDto(List<User> users) {
+                return users.stream().map(user -> toDto(user)).collect(Collectors.toList());
+        }
+
+}
