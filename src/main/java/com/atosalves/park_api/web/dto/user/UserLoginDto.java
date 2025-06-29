156a2adc1306e3ff810eed4ad1c6907e10c829ed
Atos Alves <atosalvesqueiroz@gmail.com>
2025-06-29 11:05:26 -0300

    feat: adiciona DTO para login de usuário com validações

---
+++ src/main/java/com/atosalves/park_api/web/dto/user/UserLoginDto.java
@@ -0,0 +1,9 @@
+package com.atosalves.park_api.web.dto.user;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UserLoginDto(@NotBlank(message = "O nome de usuário não deve estar vazio") String username,
+                @NotBlank(message = "A senha não deve estar vazia") @Size(min = 6, message = "A senha deve conter no mínimo 6 caracteres") String password) {
+
+}
