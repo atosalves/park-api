b1fc9a50144b908555d5d1cc545c73de56d7d3c1
Atos Alves <atosalvesqueiroz@gmail.com>
2025-06-29 11:10:33 -0300

    feat: adiciona suporte para autenticação JWT na configuração de segurança

--- src/main/java/com/atosalves/park_api/config/SpringSecurityConfig.java
+++ src/main/java/com/atosalves/park_api/config/SpringSecurityConfig.java
@@ -11,7 +11,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import com.atosalves.park_api.jwt.JwtAuthorizationFilter;
 
 @Configuration
 @EnableWebMvc
@@ -25,12 +28,12 @@ public class SpringSecurityConfig {
                                 .formLogin(form -> form.disable())
                                 .httpBasic(basic -> basic.disable())
                                 .authorizeHttpRequests(auth -> auth
-                                                .requestMatchers(HttpMethod.POST, "api/v1/users")
-                                                .permitAll()
-                                                .anyRequest()
-                                                .authenticated())
+                                                .requestMatchers(HttpMethod.POST, "api/v1/users").permitAll()
+                                                .requestMatchers(HttpMethod.POST, "api/v1/auth").permitAll()
+                                                .anyRequest().authenticated())
                                 .sessionManagement(session -> session
                                                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                                .addFilterBefore(jwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
                                 .build();
         }
 
@@ -43,4 +46,10 @@ public class SpringSecurityConfig {
         AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
                 return configuration.getAuthenticationManager();
         }
+
+        @Bean
+        JwtAuthorizationFilter jwtAuthorizationFilter() {
+                return new JwtAuthorizationFilter();
+        }
+
 }
