b5552c24ddb421bd943c9a0902f88200e63567d8
Atos Alves <atosalvesqueiroz@gmail.com>
2025-07-01 14:03:00 -0300

    fix: corrige os caminhos dos scripts SQL nos testes de autenticação

--- src/test/java/com/atosalves/park_api/AuthIT.java
+++ src/test/java/com/atosalves/park_api/AuthIT.java
@@ -15,8 +15,8 @@ import com.atosalves.park_api.web.dto.user.UserLoginDto;
 import com.atosalves.park_api.web.exception.ErrorMessage;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Sql(scripts = "/sql/users/users-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-@Sql(scripts = "/sql/users/users-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+@Sql(scripts = "/sql/users/insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = "/sql/users/delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 public class AuthIT {
 
         @Autowired
