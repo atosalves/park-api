ec6e8a3e7dc8a5e11fe7d6e9822984f5d30043e0
Atos Alves <atosalvesqueiroz@gmail.com>
2025-07-01 17:34:54 -0300

    feat: adiciona método findAllPageable para busca paginada de clientes

--- src/main/java/com/atosalves/park_api/repository/CustomerRepository.java
+++ src/main/java/com/atosalves/park_api/repository/CustomerRepository.java
@@ -1,9 +1,15 @@
 package com.atosalves.park_api.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.atosalves.park_api.entity.Customer;
+import com.atosalves.park_api.repository.projection.CustomerProjection;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+        @Query("SELECT c FROM Customer c")
+        Page<CustomerProjection> findAllPageable(Pageable pageable);
 
 }
