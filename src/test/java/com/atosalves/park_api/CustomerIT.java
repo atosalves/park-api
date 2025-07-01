f76650d85d7748d7420715dff6e5ee895e51a61c
Atos Alves <atosalvesqueiroz@gmail.com>
2025-07-01 17:31:10 -0300

    test: adiciona testes de integração para getById, getAll e getDetails de cliente

--- src/test/java/com/atosalves/park_api/CustomerIT.java
+++ src/test/java/com/atosalves/park_api/CustomerIT.java
@@ -1,7 +1,9 @@
 package com.atosalves.park_api;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import com.atosalves.park_api.web.dto.PageableDto;
 import com.atosalves.park_api.web.dto.customer.CustomerCreateDto;
 import com.atosalves.park_api.web.dto.customer.CustomerResponseDto;
 import com.atosalves.park_api.web.exception.ErrorMessage;
@@ -109,6 +112,153 @@ public class CustomerIT {
 
                 assertNotNull(responseBody);
                 assertEquals(statusCode, responseBody.getStatus());
+        }
+
+        @Test
+        public void getById_ValidId_ReturnCustomerByIdWithOkStatus() {
+                var responseBody = webTestClient
+                                .get()
+                                .uri("/api/v1/customers/11")
+                                .headers(JwtAuthentication.getHeaderAuthorization(webTestClient, "admin", "123456"))
+                                .exchange()
+                                .expectStatus()
+                                .isOk()
+                                .expectBody(CustomerResponseDto.class)
+                                .returnResult()
+                                .getResponseBody();
+
+                assertNotNull(responseBody);
+                assertEquals(11L, responseBody.id());
+                assertEquals("Gilmar Dias", responseBody.name());
+                assertEquals("77115870071", responseBody.cpf());
+        }
+
+        @Test
+        public void getById_InvalidId_ReturnErrorMessageWithNotFoundStatus() {
+                var responseBody = webTestClient
+                                .get()
+                                .uri("/api/v1/customers/0")
+                                .headers(JwtAuthentication.getHeaderAuthorization(webTestClient, "admin", "123456"))
+                                .exchange()
+                                .expectStatus()
+                                .isNotFound()
+                                .expectBody(ErrorMessage.class)
+                                .returnResult()
+                                .getResponseBody();
+
+                assertNotNull(responseBody);
+                assertEquals(404, responseBody.getStatus());
+        }
+
+        @Test
+        public void getById_AsCustomer_ReturnErrorMessageWithForbbidenStatus() {
+                var responseBody = webTestClient
+                                .get()
+                                .uri("/api/v1/customers/11")
+                                .headers(JwtAuthentication.getHeaderAuthorization(webTestClient, "customer", "123456"))
+                                .exchange()
+                                .expectStatus()
+                                .isForbidden()
+                                .expectBody(ErrorMessage.class)
+                                .returnResult()
+                                .getResponseBody();
+
+                assertNotNull(responseBody);
+                assertEquals(403, responseBody.getStatus());
+        }
+
+        @Test
+        public void getAll_AsAdmin_ReturnCustomerListWithOkStatus() {
+                var responseBody = webTestClient
+                                .get()
+                                .uri("/api/v1/customers")
+                                .headers(JwtAuthentication.getHeaderAuthorization(webTestClient, "admin", "123456"))
+                                .exchange()
+                                .expectStatus()
+                                .isOk()
+                                .expectBody(PageableDto.class)
+                                .returnResult()
+                                .getResponseBody();
+
+                assertNotNull(responseBody);
+                assertNotNull(responseBody.content());
+                assertEquals(3, responseBody.numberOfElements());
+                assertTrue(responseBody.first());
+                assertTrue(responseBody.last());
+
+                responseBody = webTestClient
+                                .get()
+                                .uri("/api/v1/customers?size=1&page=1")
+                                .headers(JwtAuthentication.getHeaderAuthorization(webTestClient, "admin", "123456"))
+                                .exchange()
+                                .expectStatus()
+                                .isOk()
+                                .expectBody(PageableDto.class)
+                                .returnResult()
+                                .getResponseBody();
+
+                assertNotNull(responseBody);
+                assertNotNull(responseBody.content());
+                assertEquals(1, responseBody.numberOfElements());
+                assertEquals(3, responseBody.totalPages());
+                assertEquals(1, responseBody.number());
+                assertFalse(responseBody.first());
+                assertFalse(responseBody.last());
+
+        }
+
+        @Test
+        public void getAll_AsCustomer_ReturnErrorMessageWithForbbidenStatus() {
+                var responseBody = webTestClient
+                                .get()
+                                .uri("/api/v1/customers")
+                                .headers(JwtAuthentication.getHeaderAuthorization(webTestClient, "customer", "123456"))
+                                .exchange()
+                                .expectStatus()
+                                .isForbidden()
+                                .expectBody(ErrorMessage.class)
+                                .returnResult()
+                                .getResponseBody();
+
+                assertNotNull(responseBody);
+                assertEquals(403, responseBody.getStatus());
+        }
+
+        @Test
+        public void getDetails_AsCustomer_ReturnCustomerWithOkStatus() {
+                var responseBody = webTestClient
+                                .get()
+                                .uri("/api/v1/customers/details")
+                                .headers(JwtAuthentication.getHeaderAuthorization(webTestClient, "customer", "123456"))
+                                .exchange()
+                                .expectStatus()
+                                .isOk()
+                                .expectBody(CustomerResponseDto.class)
+                                .returnResult()
+                                .getResponseBody();
+
+                assertNotNull(responseBody);
+                assertEquals(11L, responseBody.id());
+                assertEquals("Gilmar Dias", responseBody.name());
+                assertEquals("77115870071", responseBody.cpf());
+        }
+
+        @Test
+        public void getDetails_AsAdmin_ReturnErrorMessageWithForbbidenStatus() {
+                var responseBody = webTestClient
+                                .get()
+                                .uri("/api/v1/customers/details")
+                                .headers(JwtAuthentication.getHeaderAuthorization(webTestClient, "admin", "123456"))
+                                .exchange()
+                                .expectStatus()
+                                .isForbidden()
+                                .expectBody(ErrorMessage.class)
+                                .returnResult()
+                                .getResponseBody();
+
+                assertNotNull(responseBody);
+                assertEquals(403, responseBody.getStatus());
+
         }
 
 }
