eefe69912b1b0213642ed5bc2f3f7d60a5105184
Atos Alves <atosalvesqueiroz@gmail.com>
2025-07-01 17:34:04 -0300

    feat: adiciona interface CustomerProjection para projeção de dados de clientes

---
+++ src/main/java/com/atosalves/park_api/repository/projection/CustomerProjection.java
@@ -0,0 +1,11 @@
+package com.atosalves.park_api.repository.projection;
+
+public interface CustomerProjection {
+
+        Long getId();
+
+        String getName();
+
+        String getCpf();
+
+}
