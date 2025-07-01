3712080add2c24953f38e82cca56aeed9f3183a1
Atos Alves <atosalvesqueiroz@gmail.com>
2025-07-01 14:04:13 -0300

    feat: adiciona record CustomerCreateDto para representar a criação de clientes

---
+++ src/main/java/com/atosalves/park_api/web/dto/customer/CustomerCreateDto.java
@@ -0,0 +1,10 @@
+package com.atosalves.park_api.web.dto.customer;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CustomerCreateDto(@NotBlank @Size(min = 3, max = 100) String name,
+                @CPF @Size(max = 11) String cpf) {
+}
