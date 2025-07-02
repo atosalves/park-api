67a6bfb4f978c018ff2614a8542fb154dd9885c1
Atos Alves <atosalvesqueiroz@gmail.com>
2025-07-02 14:17:07 -0300

    feat: adiciona classe ParkingSpotService para gerenciamento de vagas de estacionamento
    - adiciona método create da classe ParkingSpotService

---
+++ src/main/java/com/atosalves/park_api/service/ParkingSpotService.java
@@ -0,0 +1,29 @@
+package com.atosalves.park_api.service;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.atosalves.park_api.entity.ParkingSpot;
+import com.atosalves.park_api.exception.UniqueViolationException;
+import com.atosalves.park_api.repository.ParkingSpotRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class ParkingSpotService {
+
+        private ParkingSpotRepository parkingSpotRepository;
+
+        @Transactional
+        public ParkingSpot create(ParkingSpot parkingSpot) {
+                try {
+                        return parkingSpotRepository.save(parkingSpot);
+                } catch (DataIntegrityViolationException e) {
+                        throw new UniqueViolationException(
+                                        String.format("Código '%s' já cadastrado", parkingSpot.getCode()));
+                }
+        }
+
+}
