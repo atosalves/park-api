bb8360898a4c1c31686e2c98ea2698c022c52340
Atos Alves <atosalvesqueiroz@gmail.com>
2025-07-02 15:27:27 -0300

    feat: adiciona método para buscar ParkingSpot por código

--- src/main/java/com/atosalves/park_api/repository/ParkingSpotRepository.java
+++ src/main/java/com/atosalves/park_api/repository/ParkingSpotRepository.java
@@ -1,9 +1,13 @@
 package com.atosalves.park_api.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.atosalves.park_api.entity.ParkingSpot;
 
 public interface ParkingSpotRepository extends JpaRepository<ParkingSpot, Long> {
+
+        Optional<ParkingSpot> findByCode(String code);
 
 }
