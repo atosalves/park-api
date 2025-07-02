07ac6171a4d8dac8b6e86e4734296eb30d799ea1
Atos Alves <atosalvesqueiroz@gmail.com>
2025-07-02 17:31:26 -0300

    feat: adiciona mensagens de validação ao ParkingSpotCreateDto

--- src/main/java/com/atosalves/park_api/web/dto/parkingSpot/ParkingSpotCreateDto.java
+++ src/main/java/com/atosalves/park_api/web/dto/parkingSpot/ParkingSpotCreateDto.java
@@ -4,7 +4,8 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
-public record ParkingSpotCreateDto(@NotBlank @Size(min = 4, max = 4) String code,
-                @NotBlank @Pattern(regexp = "AVAILABLE|OCCUPIED") String status) {
+public record ParkingSpotCreateDto(
+                @NotBlank(message = "O código não deve estar vazio") @Size(min = 4, max = 4) String code,
+                @NotBlank(message = "O status não deve estar vazio") @Pattern(regexp = "AVAILABLE|OCCUPIED") String status) {
 
 }
