5e40bd0fddb234b1782586ccd26023ce55e37326
Atos Alves <atosalvesqueiroz@gmail.com>
2025-07-02 14:16:38 -0300

    feat: adiciona classe ParkingSpotMapper para conversão entre DTOs e entidade ParkingSpot

---
+++ src/main/java/com/atosalves/park_api/web/dto/mapper/ParkingSpotMapper.java
@@ -0,0 +1,26 @@
+package com.atosalves.park_api.web.dto.mapper;
+
+import org.springframework.beans.BeanUtils;
+
+import com.atosalves.park_api.entity.ParkingSpot;
+import com.atosalves.park_api.web.dto.parkingSpot.ParkingSpotCreateDto;
+import com.atosalves.park_api.web.dto.parkingSpot.ParkingSpotResponseDto;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ParkingSpotMapper {
+
+        public static ParkingSpot toUser(ParkingSpotCreateDto createDto) {
+                var parkingSpot = new ParkingSpot();
+                BeanUtils.copyProperties(createDto, parkingSpot);
+                return parkingSpot;
+        }
+
+        public static ParkingSpotResponseDto toDto(ParkingSpot parkingSpot) {
+                return new ParkingSpotResponseDto(parkingSpot.getId(), parkingSpot.getCode(),
+                                parkingSpot.getStatus().name());
+        }
+
+}
