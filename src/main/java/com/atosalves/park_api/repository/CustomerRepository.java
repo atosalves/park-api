47aff517479eb91446a4082917684cfb4e0554e5
Atos Alves <atosalvesqueiroz@gmail.com>
2025-07-04 11:51:30 -0300

    feat: adiciona método para buscar cliente por CPF no repositório CustomerRepository

--- src/main/java/com/atosalves/park_api/repository/CustomerRepository.java
+++ src/main/java/com/atosalves/park_api/repository/CustomerRepository.java
@@ -1,5 +1,7 @@
 package com.atosalves.park_api.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,4 +15,6 @@ public interface CustomerRepository extends JpaRepository<Customer, Long> {
         Page<CustomerProjection> findAllPageable(Pageable pageable);
 
         Customer findByUserId(Long id);
+
+        Optional<Customer> findByCpf(String cpf);
 }
