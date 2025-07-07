f1694b358364534a14b27e3f6434e39809f5d9cb
Atos Alves <atosalvesqueiroz@gmail.com>
2025-07-07 12:14:56 -0300

    feat: adiciona endpoint para buscar todos os estacionamentos de um cliente

--- src/main/java/com/atosalves/park_api/web/controller/ParkingController.java
+++ src/main/java/com/atosalves/park_api/web/controller/ParkingController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.atosalves.park_api.entity.CustomerParkingSpot;
+import com.atosalves.park_api.jwt.JwtUserDetails;
 import com.atosalves.park_api.service.CustomerParkingSpotService;
 import com.atosalves.park_api.service.ParkingService;
 import com.atosalves.park_api.web.dto.PageableDto;
@@ -72,7 +74,15 @@ public class ParkingController {
         public ResponseEntity<PageableDto<CustomerParkingSpot>> getAllByCpf(@PathVariable String cpf,
                         @PageableDefault(size = 5, sort = "entryAt", direction = Sort.Direction.ASC) Pageable pageable) {
                 var projection = customerParkingSpotService.getAllByCpf(cpf, pageable);
+                return ResponseEntity.ok(PageableMapper.toDto(projection));
+        }
 
+        @GetMapping
+        @PreAuthorize("hasRole('CUSTOMER')")
+        public ResponseEntity<PageableDto<CustomerParkingSpot>> getAllByCustomerUserId(
+                        @AuthenticationPrincipal JwtUserDetails user,
+                        @PageableDefault(size = 5, sort = "entryAt", direction = Sort.Direction.ASC) Pageable pageable) {
+                var projection = customerParkingSpotService.getAllById(user.getId(), pageable);
                 return ResponseEntity.ok(PageableMapper.toDto(projection));
         }
 
