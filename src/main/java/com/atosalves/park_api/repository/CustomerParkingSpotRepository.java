c8dcfd887f9a61637d2c7c61e31a836c110c84ae
Atos Alves <atosalvesqueiroz@gmail.com>
2025-07-07 11:23:23 -0300

    feat: adiciona métodos de busca ao CustomerParkingSpotRepository

--- src/main/java/com/atosalves/park_api/repository/CustomerParkingSpotRepository.java
+++ src/main/java/com/atosalves/park_api/repository/CustomerParkingSpotRepository.java
@@ -1,9 +1,19 @@
 package com.atosalves.park_api.repository;
 
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.atosalves.park_api.entity.CustomerParkingSpot;
 
 public interface CustomerParkingSpotRepository extends JpaRepository<CustomerParkingSpot, Long> {
+
+        Optional<CustomerParkingSpot> findByReceiptAndExitAtIsNull(String receipt);
+
+        long countByCustomerCpfAndExitAtIsNotNull(String cpf);
+
+        Page<CustomerParkingSpot> findAllByCustomerCpf(String cpf, Pageable pageable);
 
 }
